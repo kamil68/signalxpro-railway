7f92b0f9179672b3af3d8684fd9612e00ec8d018
KAMiL <35708915+kamil68@users.noreply.github.com>
2025-06-10 02:43:11 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات شما فعال شد ✅")
 
 if name == "__main__":
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(7682167976:AAGNWMFuhevOus0mexZSRs2ZgFlFUJ-Kufk).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
