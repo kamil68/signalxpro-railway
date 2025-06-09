2787e646de198e4b66783a49291c636975d08144
kamil68 <35708915+kamil68@users.noreply.github.com>
2025-06-10 01:58:45 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات SignalXPro آماده‌ست 🚀")
 
-if name == "main":
+if name == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
